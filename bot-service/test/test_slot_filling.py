6957ce71984e38c2121192387d795fc024ca87ec
TM_Lechik <aleshaboyko30@yandex.ru>
2026-05-25 15:12:00 +0300

    Add test script for RuBert slot filling service

---
+++ bot-service/test/test_slot_filling.py
@@ -0,0 +1,28 @@
+from pathlib import Path
+import sys
+
+BOT_SERVICE_DIR = Path(__file__).resolve().parents[1]
+sys.path.append(str(BOT_SERVICE_DIR))
+
+from services.slot_filling_service import RuBertSlotFillingService
+
+def main() -> None:
+    model_path = BOT_SERVICE_DIR / "ml" / "models" / "rubert-slot-filling"
+    service = RuBertSlotFillingService(model_path)
+
+    text = [
+        "Передай Алану Султангарееву, что он молодец",
+        "Алан Султангареев, настрой телегу и всякие бэкенд штучки и зайди на пм-пу посел 18.00",
+        "Передай Алану Султангарееву, что он должен поговорить с Сашей Романченко по поводу закрытия таски",
+        ]
+    
+    for i in range(len(text)):
+        result = service.extract(text[i])
+
+        print("Text:", text[i])
+        print("Recipient:", result.recipient)
+        print("Message:", result.message)
+
+
+if __name__ == "__main__":
+    main()
